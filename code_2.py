36055fed749a54fb5ba51c88f77e6dcaeca599bc
Roman Lazarev <144658461+romalazar@users.noreply.github.com>
2023-11-20 20:27:37 +0200

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,51 @@
+def main():
+import random
+    return data
+if __name__ == "__main__":
+
+        print(f"Random Number: {item}")
+def generate_random_data():
+    main()
+    data = generate_random_data()
+    data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:
+
+
+
+if __name__ == "__main__":
+
+def main():
+        print(f"Random Number: {item}")
+
+def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
+import random
+
+    for item in data:
+    main()
+    return data
+    data = generate_random_data()public class RandomDataGenerator {
+        int[] data = new int[10];
+    public static void main(String[] args) {
+        for (int item : data) {
+        }
+    }
+}
+            System.out.println("Random Number: " + item);
+
+            data[i] = (int) (Math.random() * 100) + 1;
+
+        for (int i = 0; i < 10; i++) {
+        }
+if __name__ == "__main__":
+def generate_random_data():
+def main():
+    data = [random.randint(1, 100) for _ in range(10)]
+import random
+    for item in data:
+
+    data = generate_random_data()
+    main()
+        print(f"Random Number: {item}")
+    return data
+
